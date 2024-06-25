accd8ca2e35555375988984fa26ee5061ee3fa2b
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-06-25 14:32:55 +0400

    Updated in 'Khalifah A City, AE' on BRANCH 'react-2-typescript' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  package-lock.json package.json src/App.tsx src/index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import NavBar from './components/NavBar';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Typrescript
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
